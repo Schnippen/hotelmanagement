4ef7338b4e2162a30b98d529533d8d02eb7a3adc
Schnippen <dfojcik35@gmail.com>
2024-03-16 17:13:46 +0100

    working on new screen

---
+++ Screens/AvailableRoomsScreen.tsx
@@ -0,0 +1,13 @@
+import { Text } from '@rneui/themed'
+import React from 'react'
+import { View } from 'react-native'
+
+function AvailableRoomsScreen() {
+  return (
+    <View style={{flex:1,}}>
+        <Text>ddsdadsddas, here available rooms in selected dates range</Text>
+    </View>
+  )
+}
+
+export default AvailableRoomsScreen
