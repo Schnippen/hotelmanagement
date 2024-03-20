cd4d07ea886354b820a5f9a17ec6b5b37374d4e6
Schnippen <dfojcik35@gmail.com>
2024-03-20 23:37:37 +0100

    added links to check out

--- Supabase/supabase.ts
+++ Supabase/supabase.ts
@@ -1,12 +1,11 @@
-
-  import 'react-native-url-polyfill/auto'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createClient } from '@supabase/supabase-js'
-import {REACT_NATIVE_SUPABASE_URL,REACT_NATIVE_SUPABASE_ANON_KEY} from "@env"
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {createClient} from '@supabase/supabase-js';
+import {REACT_NATIVE_SUPABASE_URL, REACT_NATIVE_SUPABASE_ANON_KEY} from '@env';
 
 //create  ENV  for this
-const supabaseUrl = REACT_NATIVE_SUPABASE_URL
-const supabaseAnonKey = REACT_NATIVE_SUPABASE_ANON_KEY
+const supabaseUrl = REACT_NATIVE_SUPABASE_URL;
+const supabaseAnonKey = REACT_NATIVE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
@@ -15,4 +14,8 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     persistSession: true,
     detectSessionInUrl: false,
   },
-}) 
+});
+
+//https://supabase.com/blog/react-native-authentication
+//https://www.npmjs.com/package/aes-js
+//https://www.npmjs.com/package/react-native-get-random-values
