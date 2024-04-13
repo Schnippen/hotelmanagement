349f665a442d965e53e06a3e1fdf5438959b7b5a
Schnippen <dfojcik35@gmail.com>
2024-04-14 01:04:10 +0200

    compass is done

--- Utils/mahjong.tsx
+++ Utils/mahjong.tsx
@@ -1,6 +1,6 @@
 import { Image, Text } from "@rneui/themed"
 import React from "react"
-import { View } from "react-native"
+import { ScrollView, View } from "react-native"
 import { mahjongTilesSVGsArray } from "./MahjongTiles/MahjongTiles" 
 import { SvgXml } from "react-native-svg"
 
@@ -75,30 +75,76 @@ const PlayersHandComponent=()=>{
 
 const Compass = () => {
     //measuring from screenshot as a scale of reference
-    const compassBottomPerimeter = 320
+    const compassBottomPerimeter = 320 //320-200=120 
     const compassTilesCounterBottomPerimeter=+(100 * 1.0188).toFixed(2)
     const compassTurnIndicatorBottomPerimeter=200
+    const backgroundColor= "#5a5a66"
+    const backgroundColorSec="#2f2f39"
     const CompassTileCounter=()=>{//317  /100 //center piece
         return(
-            <View style={{backgroundColor:"pink",width:compassTilesCounterBottomPerimeter,height:compassTilesCounterBottomPerimeter,justifyContent:"space-around",}}>
-                <Text>EAST 3</Text>
-                <Text style={{backgroundColor:"lightblue",height:36}}>TILES LEFT</Text>
+            
+            <View style={{backgroundColor:"#1d1d1f",width:compassTilesCounterBottomPerimeter,height:compassTilesCounterBottomPerimeter,alignItems:"center",borderBottomWidth:1,borderBottomColor:"#1b2a2d"}}>
+                <Text style={{flex:1,fontSize:22,textAlign:"center",width:"100%",textAlignVertical:"center",color:"#4affff"}}>EAST 3</Text>
+                <Text style={{backgroundColor:"lightblue",flex:1,fontSize:40,width:"100%",textAlign:"center",color:"#4affff"}}>69</Text>
             </View>
         )
     }
     const CompassTurnIndicator=()=>{
+        const TriangleRight=()=>{return(
+            <View style={ {
+                width: 0,
+                height: 0,
+                borderLeftWidth: 0,
+                borderRightWidth: 50,
+                borderBottomWidth: 40,
+                borderTopWidth:0,
+                borderStyle: 'solid',
+                backgroundColor: 'transparent',
+                borderLeftColor: 'transparent',
+                borderRightColor: 'transparent',
+                borderBottomColor: "lime",
+                borderTopColor: "lime",
+              position:"absolute",top:0,right:0
+              }}></View>
+        )}
+        const TriangleLeft=()=>{return(
+            <View style={ {
+                width: 0,
+                height: 0,
+                borderLeftWidth: 50,
+                borderRightWidth: 0,
+                borderBottomWidth: 40,
+                borderTopWidth:0,
+                borderStyle: 'solid',
+                backgroundColor: 'transparent',
+                borderLeftColor: 'transparent',
+                borderRightColor: 'transparent',
+                borderBottomColor: "lime",
+                borderTopColor: "lime",
+              position:"absolute",top:0,left:0
+              }}></View>
+        )}
+        const containerWidth=200
         return(
-            <View style={{width:compassTurnIndicatorBottomPerimeter,height:40,backgroundColor:"orange",}}>
-                <View style={{backgroundColor:"lime", height:10,}}></View>
-                <View style={{width:85,height:20,backgroundColor:"gray"}}>
-                <Text>SCORE</Text>
-                </View>
+            <View style={{width:200,height:40,backgroundColor:"transparent",position:"relative",justifyContent:"flex-end"}}>
+                
+                        <View style={{height:70,width:containerWidth,backgroundColor:"transparent",bottom:0,position:'absolute'}}>
+                            <View style={{width:containerWidth,position:'relative'}}>
+                            <View style={{position:"absolute",height:40,width:100,backgroundColor:"lime", top:0,left:50}}/>
+                            <TriangleRight/>
+                            <TriangleLeft/>
+                            </View>
+
+                        </View> 
+                        <View style={{backgroundColor:"#39383d",height:30}}>
+                            <Text style={{fontSize:20,color:"#ffdb51",textAlignVertical:"center",textAlign:"center"}}>25000</Text>
+                        </View>
             </View>
         )
     }
     const CompassRichiiIndicator=()=>{
         return(
-            <View style={{width:200,height:30,justifyContent:"center",backgroundColor:"gray",alignItems:'center',}}>
+            <View style={{width:200,height:30,justifyContent:"center",backgroundColor:"#5d5d69",alignItems:'center',}}>
                 <View style={{backgroundColor:"black",height:20,width:150,borderRadius:20}}></View>
             </View>
         )
@@ -108,22 +154,30 @@ const Compass = () => {
             <View style={ {
                 width: 0,
                 height: 0,
-                borderLeftWidth: 37,
+                borderLeftWidth: 30,
                 borderRightWidth: 0,
                 borderBottomWidth: 0,
-                borderTopWidth:25,
+                borderTopWidth:30,
                 borderStyle: 'solid',
                 backgroundColor: 'transparent',
                 borderLeftColor: 'transparent',
                 borderRightColor: 'transparent',
-                borderBottomColor: '#606070',
-                borderTopColor: '#606070',
+                borderBottomColor: "#39383d",
+                borderTopColor: "#39383d",
               position:"absolute",top:0,right:0
               }}></View>
         )}
-        return(//65 45
-            <View style={{borderBottomLeftRadius:8,width:65,height:60,backgroundColor:"green",position:"absolute",bottom:0,left:0}}>
+        //16=south
+        //17=north
+        //28=west
+        //39=east #bc2f38
+        let index =28
+        let defaultWindBackground="beige"
+        let currentWindBackground=index===39?"#bc2f38":defaultWindBackground
+        return(//65 45 
+            <View style={{borderBottomLeftRadius:8,width:60,height:60,backgroundColor:currentWindBackground,position:"absolute",bottom:0,left:0,justifyContent:"flex-end",alignItems:'flex-start'}}>
                 <Triangle/>
+                <SvgXml width={50} height={50} xml={mahjongTilesSVGsArray[index]} style={{marginLeft:2,marginBottom:2}} />
             </View>
         )
     }
@@ -133,7 +187,7 @@ const Compass = () => {
         return(
         <View style={{backgroundColor:"transparent",flexDirection:"row",height:70,justifyContent:"center",width:compassBottomPerimeter,transform: [{rotate: `${degrees}deg`}],position:"absolute",top:rightPosition,left:leftPosition,right:rightPosition,bottom:bottomPosition}}>
         <CompassWindIndicator/>
-            <View style={{flexDirection:"column"}}>
+            <View style={{flexDirection:"column",height:70,backgroundColor:"transparent"}}>
             <CompassTurnIndicator/>
             <CompassRichiiIndicator/>
             </View>
@@ -144,15 +198,16 @@ const Compass = () => {
     //transform: [{rotateX: '45deg'}]
     return (
       <View style={{
-        backgroundColor:"red",
+        backgroundColor:"#39383d",
         width: compassBottomPerimeter,  //
         height:compassBottomPerimeter, //padding 8   8 45
         position:"relative",
         justifyContent:"center",
-        alignItems:"center"
+        alignItems:"center",
+        borderRadius:8,
       }} >
         <CompassTileCounter/> 
-        <PlayerSide degrees={0} leftPosition={0} rightPosition={250} bottomPosition={0}  topPosition={0}/> 
+         <PlayerSide degrees={0} leftPosition={0} rightPosition={250} bottomPosition={0}  topPosition={0}/> 
         <PlayerSide degrees={90} leftPosition={-125} rightPosition={125} bottomPosition={0}  topPosition={0}/> 
         <PlayerSide degrees={180} leftPosition={0} rightPosition={0} bottomPosition={0}  topPosition={0}/>
         <PlayerSide degrees={270} leftPosition={125} rightPosition={125} bottomPosition={0}  topPosition={0}/> 
@@ -162,10 +217,13 @@ const Compass = () => {
 
 function MahjongScreen({navigation, route}: any) {
     return(
-        <View style={{flex:1,justifyContent:'center',alignItems:"center"}}>
-{/*<PlayersHandComponent/>*/}
+        <ScrollView style={{flex:1}}>
+            <View style={{borderBottomWidth:8,borderRadius:8, transform: [{rotateX: '45deg'}, {rotateZ: '0deg'},{scale:0.8}],backgroundColor:"#5d5d69",width:320}}>
             <Compass/>
-        </View>
+            </View>
+            <PlayersHandComponent/>
+
+        </ScrollView>
     )
 }
 export default MahjongScreen;
