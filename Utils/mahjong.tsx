36f48cc61052f273bfb2dd22a3d94e5d953f502d
Schnippen <dfojcik35@gmail.com>
2024-04-21 00:16:09 +0200

    x

--- Utils/mahjong.tsx
+++ Utils/mahjong.tsx
@@ -73,8 +73,7 @@ const TileComponent =({svg,tileRatioProp=3}:{svg:string,tileRatioProp:number})=>
 
 const RichiiStick =({degrees}:{degrees:number})=>{
   //use transform on it
-  //TODO create styles dependend on perspective
-  const position = degrees===0?"front":degrees===90?"right":degrees===180?"top":degrees===270?"left":null //130 /126 /18 /14  chage to /18 /16
+  const position = degrees===0?"front":degrees===90?"right":degrees===180?"top":degrees===270?"left":null 
   return(     
   <View style={{width:130,backgroundColor:"#bdbbc0",height:18,borderWidth:1,borderRightWidth:2,borderLeftWidth:2,borderRadius:6,}}>
     <View style={{backgroundColor:"#e9ebe8",width:position==="front"||"top"?126:124,height:position==="front"||"top"?14:16,alignItems:"center",justifyContent:"center",alignSelf:position==="front"||"top"?"center":"flex-end",borderRadius:4}}>
